ebf961c01395d416fc35a198542810c116665e76
Frederik Bennhoff <frederik@bennhoff.de>
2023-12-03 12:58:34 +0100

    fix issues with signal being too short

--- project_2/src/prepare_stft_sgrams.py
+++ project_2/src/prepare_stft_sgrams.py
@@ -9,12 +9,18 @@ import argparse
 
 ## Functions
 # get the spectrogram figure
-def get_spectrogram(f, t, Zxx, nsec=10, segment=0, show=False):
+def get_spectrogram(f, t, Zxx, nsec=8.15, segment=0, show=False):
     
     t_lo = nsec*(segment)+1.5 # add 1.5 to account for the window size
     t_up = nsec*(segment+1)+1.5
     if t_up > max(t):
-        return None
+        if segment == 0:
+            t_lo = nsec*(segment)
+            t_up = nsec*(segment+1)
+            if t_up > max(t):
+                return None
+        else:
+            return None
     fig, ax = plt.subplots(1,1, figsize=(3, 3));  # Create a figure
     ax.pcolormesh(t, f, Zxx, shading='gouraud', cmap='jet');
     ax.set_xticklabels("");
@@ -41,7 +47,7 @@ def save_spectrograms(index, labels_array, data, folder, M=256, sampling_rate=30
     f, t, Zxx = stft(ecg_data, fs=sampling_rate, window=hann(M), nperseg=M)
     figs = []
     for seg in range(10):
-        fig = get_spectrogram(f, t, np.log(np.abs(Zxx)+.0001), nsec=10, segment=seg, show=show);
+        fig = get_spectrogram(f, t, np.log(np.abs(Zxx)+.0001), nsec=8.15, segment=seg, show=show); # note that nsec is less than the shortest signal in dataset
         if fig is None:
             break
         figs.append(fig)
@@ -60,36 +66,48 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
     # Add the arguments
     parser.add_argument('M', type=int, help='window size of stft')
+    parser.add_argument('datatype', type=str, help="type of data requested: 'training' or 'test' ")
     # Parse the arguments
     args = parser.parse_args()
-
-    ## Load data and labels
-    data_folder = "data/processed/"
-    labels = pd.read_csv(data_folder+'y_train.csv', index_col='id') # read labels
-    data = pd.read_csv(data_folder+'/X_train.csv', index_col='id') # read data
-    labels_array = labels['y'].to_numpy()
-    data_test = pd.read_csv(folder+'/X_test.csv', index_col='id') # read test data
-    labels_array_test = np.empty(data_test.shape[0], dtype=object) # dummy labels for test data
-    labels_array_test[:] = "test"
-    labels_array_test
 
     M = args.M # window size
     sampling_rate = 300
 
+    data_folder = "data/processed/"
+    folder = f"data/spectrograms_{M}/"
+
+    ## Load data and labels
+    if args.datatype == 'training':
+        labels = pd.read_csv(data_folder+'y_train.csv', index_col='id') # read labels
+        data = pd.read_csv(data_folder+'/X_train.csv', index_col='id') # read data
+        labels_array = labels['y'].to_numpy()
+
+    if args.datatype == 'test':
+        data_test = pd.read_csv(data_folder+'/X_test.csv', index_col='id') # read test data
+        labels_array_test = np.empty(data_test.shape[0], dtype=object) # dummy labels for test data
+        labels_array_test[:] = "test"
+        labels_array_test
+
     ## create and save training and test data
-    print("Creating training data: ")
-    for index in range(len(data)):
-        if not (f"{(index+1)/len(data):.0%}" == f"{index/len(data):.0%}"):
-            print(f"{index/len(data):.0%} ", end="")
-        folder = f"data/spectrograms_{M}/"
-        save_spectrograms(index, labels_array, data, folder, M=M, sampling_rate=sampling_rate, show=False);
+    if args.datatype == 'training':
+        print("Creating training data: ")
+        for index in range(len(data)):
+            if not (f"{(index+1)/len(data):.0%}" == f"{index/len(data):.0%}"):
+                print(f"{index/len(data):.0%} ", end="")
+            save_spectrograms(index, labels_array, data, folder, M=M, sampling_rate=sampling_rate, show=False);
+        
+        # Save a text file with a completion message and timestamp
+        with open(folder+"done.txt", 'w') as f:
+            f.write('Done at {timestamp}'.format(timestamp=datetime.now()))
     
     print("Creating test data: ")
-    for index in range(len(data_test)):
-        if not (f"{(index+1)/len(data_test):.0%}" == f"{index/len(data_test):.0%}"):
-            print(f"{index/len(data_test):.0%} ", end="")
-        save_spectrograms(index, labels_array_test, data, folder, M=256, sampling_rate=300, show=False);
+    if args.datatype == 'test':
+        for index in range(len(data_test)):
+            if not (f"{(index+1)/len(data_test):.0%}" == f"{index/len(data_test):.0%}"):
+                print(f"{index/len(data_test):.0%} ", end="")
+            save_spectrograms(index, labels_array_test, data_test, folder, M=M, sampling_rate=sampling_rate, show=False);
 
-    ## Save a text file with a completion message and timestamp
-    with open(folder+"done.txt", 'w') as f:
-        f.write('Done at {timestamp}'.format(timestamp=datetime.now()))
+        # Save a text file with a completion message and timestamp
+        with open(folder+"done_testdata.txt", 'w') as f:
+            f.write('Done at {timestamp}'.format(timestamp=datetime.now()))
+    
